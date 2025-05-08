9c947d55898c36d21ad64b0947a3721696130f68
dnzoncuu <berkoncu17@gmail.com>
2025-05-08 10:36:44 +0300

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Merhaba,Ben Deniz Berk Öncü");
+Console.WriteLine("Yazılım Dünyasına girerken bana bu yolda yardımcı olacaklarına inandıgım için patika bootcampteyim!");
